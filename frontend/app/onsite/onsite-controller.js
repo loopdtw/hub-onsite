dee77546b5e16655c47b6a9a9be9185d86dad42b
Allen Houng <allen@getloopd.com>
2016-04-17 05:12:05 +0800

    added window dep

--- frontend/app/onsite/onsite-controller.js
+++ frontend/app/onsite/onsite-controller.js
@@ -1,5 +1,5 @@
 angular.module('HubApp')
-    .controller('onsiteController', function($scope, $http, $timeout, $q, utilService, socketService, badgeService, attendeeService, $location) {
+    .controller('onsiteController', function($scope, $http, $timeout, $q, utilService, socketService, badgeService, attendeeService, $location, $window) {
 
         /*----------  VAR DECLARATIONS  ----------*/
 
