5eb07f3dce5519a9c09182c69d3591be8e7fc292
Allen Houng <allen@getloopd.com>
2017-01-06 14:27:18 +0800

    updated run to untar

--- run.sh
+++ run.sh
@@ -2,5 +2,10 @@
 
 set -x #echo on
 
+if [ ! -d node_modules ]; then
+	mkdir node_modules
+	tar -czvf node_modules_edison.tar.gz ./node_modules
+fi
+
 /usr/bin/git pull
 /usr/bin/npm run edison:production
